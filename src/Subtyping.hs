999308a49506e0d6cc0eb7839698525f192cd0f1
hank <ysh2291939848@outlook.com>
2025-06-21 17:31:04 +0800

    update

--- src/Subtyping.hs
+++ src/Subtyping.hs
@@ -1,1 +1,17 @@
 module Subtyping where
+
+import Data.Map as M
+
+data Type
+  = TRecord [(String, Type)]
+  | TTop
+  | TArr Type Type
+
+data Term
+  = Record 
+
+newtype Context = Context (M.Map String Binding)
+
+data Binding
+  = NameBind
+  | VarBind Type
